aed0852d0eea5b6ea28a08cc37e76ede9bc99b0f
Elb1to <elbitomc@gmail.com>
2023-11-28 21:43:25 -0500

    feat: defined lobby inventory

--- src/main/java/me/elb1to/ffa/util/PlayerUtil.java
+++ src/main/java/me/elb1to/ffa/util/PlayerUtil.java
@@ -1,9 +1,13 @@
 package me.elb1to.ffa.util;
 
 import lombok.experimental.UtilityClass;
+import me.elb1to.ffa.util.item.ItemBuilder;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 
 /**
@@ -13,7 +17,13 @@ import org.bukkit.potion.PotionEffect;
 @UtilityClass
 public class PlayerUtil {
 
-	public void clearPlayer(Player player) {
+	private final ItemStack joinItem = new ItemBuilder(Material.COMPASS).name("&dJoin FFA &7(Right Click)").build();
+	private final ItemStack settingsItem = new ItemBuilder(Material.DIODE).name("&dSettings &7(Right Click)").build();
+	private final ItemStack leaderboardItem = new ItemBuilder(Material.EMERALD).name("&dLeaderboards &7(Right Click)").build();
+	private final ItemStack editorItem = new ItemBuilder(Material.BOOK).name("&dEdit Kits &7(Right Click)").build();
+	private final ItemStack spectateItem = new ItemBuilder(Material.PAINTING).name("&dSpectate FFA &7(Right Click)").build();
+
+	public void clearPlayer(Player player, boolean toSpawn, Location location) {
 		player.closeInventory();
 		player.setFireTicks(0);
 		player.setHealth(20.0D);
@@ -32,6 +42,16 @@ public class PlayerUtil {
 		player.setMaximumNoDamageTicks(20);
 		player.getActivePotionEffects().stream().map(PotionEffect::getType).forEach(player::removePotionEffect);
 		((CraftPlayer) player).getHandle().getDataWatcher().watch(9, (byte) 0);
+
+		if (toSpawn && location != null) {
+			player.teleport(location);
+			player.getInventory().setItem(0, joinItem);
+			player.getInventory().setItem(1, settingsItem);
+			player.getInventory().setItem(4, leaderboardItem);
+			player.getInventory().setItem(7, editorItem);
+			player.getInventory().setItem(8, spectateItem);
+		}
+
 		player.updateInventory();
 	}
 
